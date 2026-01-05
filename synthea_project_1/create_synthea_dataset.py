ed7a80ff17d8ba26042b66a6edbd0fabd7ef361a
kshukla85 <45736379+kshukla85@users.noreply.github.com>
2026-01-05 19:05:42 +0000

    found number of encounters for patients

--- synthea_project_1/create_synthea_dataset.py
+++ synthea_project_1/create_synthea_dataset.py
@@ -1,6 +1,29 @@
 import pandas as pd
+import duckdb
+import numpy as np
+
+# Set pandas display options to show all columns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 df1 = pd.read_csv("allergies.csv")
 df2 = pd.read_csv("careplans.csv")
 print(df1.head())
 print(df2.head())
+
+con = duckdb.connect()
+
+query = """
+SELECT
+    COUNT(ENCOUNTER) AS total_encounters
+FROM read_csv_auto('allergies.csv')
+"""
+
+df1 = con.execute(query).fetchdf()
+print("Column names:", list(df1.columns))
+print(df1)
+
+# Test numpy
+print("NumPy version:", np.__version__)
+print("NumPy array test:", np.array([1, 2, 3]))
