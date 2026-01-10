7135736b5a0ad05304bccddef31c899b14f0540e
kshukla85 <45736379+kshukla85@users.noreply.github.com>
2026-01-10 01:05:56 +0000

    adding new info for results on multiple diagnoses and why

--- tempus_assessment/queries.sql
+++ tempus_assessment/queries.sql
@@ -10,22 +10,3 @@ FROM (
     HAVING COUNT(diagnosis_id) > 1
 );
 
--- b & c. Show details of patients with multiple diagnoses
-SELECT 
-    d.patient_id,
-    p.gender,
-    p.birth_date,
-    p.country,
-    d.diagnosis_id,
-    d.cancer_type,
-    d.stage,
-    d.diagnosis_date
-FROM Diagnosis d
-JOIN Patient p ON d.patient_id = p.patient_id
-WHERE d.patient_id IN (
-    SELECT patient_id
-    FROM Diagnosis
-    GROUP BY patient_id
-    HAVING COUNT(diagnosis_id) > 1
-)
-ORDER BY d.patient_id, d.diagnosis_date;
