917c006c321c6440be416c9425bf393cc9ecac7f
kshukla85 <45736379+kshukla85@users.noreply.github.com>
2026-01-13 19:13:36 +0000

    update sql treatment.sql code

--- assessment/2020_patient_treament.sql
+++ assessment/2020_patient_treament.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(d.patient_id) AS patient_count
 FROM treatment t
 INNER JOIN diagnosis d ON t.diagnosis_id = d.diagnosis_id
-WHERE t.start_date >= '2020-01-01' or d.diagnosis_date >= '2020-01-01';
+WHERE t.start_date >= '2020-01-01';
