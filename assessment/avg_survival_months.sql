49d9b7ec786f689e7febdc52df8846f7f561e1be
kshukla85 <45736379+kshukla85@users.noreply.github.com>
2026-01-13 02:33:27 +0000

    updating the readme and my sql file

--- assessment/avg_survival_months.sql
+++ assessment/avg_survival_months.sql
@@ -3,3 +3,9 @@ d.cancer_type
 FROM Outcomes as o
 INNER JOIN Diagnosis d ON o.diagnosis_id = d.diagnosis_id
 GROUP BY d.cancer_type;
+
+SELECT AVG(o.survival_months) AS avg_survival_months,
+d.cancer_type, status
+FROM Outcomes as o
+INNER JOIN Diagnosis d ON o.diagnosis_id = d.diagnosis_id
+GROUP BY d.cancer_type, status
